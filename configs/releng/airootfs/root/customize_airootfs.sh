1247452754d9171f1f3b6defe073d685fd9020c0
Jeremy J Wakeman <jeremywakeman@gmail.com>
2019-11-19 19:19:55 +0000

    updated i3 config, screenrc. added kismet, ettercap

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -9,6 +9,8 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 usermod -s /bin/bash root
 cp -aT /etc/skel/ /root/
+sed -i "/runloop/d" /root/.screenrc
+sed -i "/runmain/d" /root/.screenrc
 chmod 700 /root
 echo "root:root" | chpasswd
 
@@ -18,10 +20,13 @@ gpasswd -a snal wheel
 gpasswd -a snal audio
 gpasswd -a snal video
 cp -aT /etc/skel/ /home/snal/
+sed -i "s/#runloop/\/home\/snal\/.bin\/runloop/" /home/snal/.screenrc
+sed -i "s/#runmain/\/home\/snal\/.bin\/runmain/" /home/snal/.screenrc
 chown -R snal:users /home/snal
 chmod 700 /home/snal
 echo "snal:snal" | chpasswd
 
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
