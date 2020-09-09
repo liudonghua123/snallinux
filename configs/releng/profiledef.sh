4ccd7cd380255a22697f04ae83458485d83532cf
Jeremy J Wakeman <jeremywakeman@gmail.com>
2020-09-08 23:09:50 -0400

    migrating snal customizations to mkarchiso: hostname localtime os-release motd passwd group profiledef.sh

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date +%Y.%m.%d)"
-install_dir="arch"
+BUILD="0.9"
+DATE=`date "+%B %e, %Y"`
+
+app_name="snal"
+iso_name="snal"
+iso_label="SNAL_$BUILD"
+iso_publisher="Snal Linux <https://sourceforge.net/projects/snallinux/>"
+iso_application="Snal Linux"
+iso_version="$BUILD"
+install_dir="snal"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
