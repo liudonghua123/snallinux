c30f7fea0c016b2ecaa7a6fd03f2c6fa5298f570
Jeremy J Wakeman <jeremywakeman@gmail.com>
2020-09-10 23:29:39 -0400

    changed install_dir and iso_name to automatically match app_name

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -5,12 +5,12 @@ BUILD="0.9"
 DATE=`date "+%B %e, %Y"`
 
 app_name="snallinux"
-iso_name="snallinux"
+iso_name="${app_name}"
 iso_label="SNAL_$BUILD"
 iso_publisher="Snal Linux <https://sourceforge.net/projects/snallinux/>"
 iso_application="Snal Linux"
 iso_version="$BUILD"
-install_dir="snal"
+install_dir="${app_name}"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
